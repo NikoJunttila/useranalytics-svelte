31f7650736367c0a393b2e994a78512aab1dd8d4
NikoJunttila <89527972+NikoJunttila@users.noreply.github.com>
2024-04-19 12:41:03 +0300

    should do env

--- src/lib/js/endpoints.js
+++ src/lib/js/endpoints.js
@@ -1,6 +1,6 @@
 //koyeb stuff
-/* export const endpoint = "https://analytics-derp.koyeb.app"
-export const exampleID = "e325ccdb-284b-4f74-bfcd-c014c300a3c9"  */
+export const endpoint = "https://analytics-derp.koyeb.app"
+export const exampleID = "e325ccdb-284b-4f74-bfcd-c014c300a3c9" 
 //LOCALHOST id below
-export const endpoint = "http://localhost:8000"
-export const exampleID = "9c698b28-4a0c-49e2-815d-0ab446088352"
+/* export const endpoint = "http://localhost:8000"
+export const exampleID = "9c698b28-4a0c-49e2-815d-0ab446088352" */
